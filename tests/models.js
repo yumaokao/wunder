9c614d49c1d59b30d43fede22cc95f28a89e4d1e
yumaokao <ymkq9h@gmail.com>
2015-12-15 11:20:42 +0800

    complete test/models.js to wrapper returned objects

--- tests/models.js
+++ tests/models.js
@@ -2,10 +2,22 @@
 
 'use strict';
 
-var WunderRoot = require('../libs/WunderRoot');
+function Obj(obj) {
+  this.obj = obj;
+};
 
-var root = new WunderRoot();
-root.fetch().then(function(r) {
-  r.lists.forEach(function(elem, index, array) { console.log(elem.title); });
-});
+Obj.prototype.show = function() {
+  console.log("show TITLE " + this.obj.title + " ID " + this.obj.id);
+};
+
+var objs = [
+  {'title': 'A', 'id': 'id_a'},
+  {'title': 'B', 'id': 'id_b'},
+  {'title': 'C', 'id': 'id_c'},
+  {'title': 'D', 'id': 'id_d'}
+];
+
+// objs.forEach(function(o) { console.log(o.title); });
+objs.map(function(o) { return new Obj(o); }).forEach(function(o) { o.show(); });
+
 // vim:fileencoding=UTF-8:ts=2:sw=2:sta:et:sts=2:ai
